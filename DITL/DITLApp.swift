0a6185b86c3d9339fd10eda2b81e155f801f4696
Akshita Santra <akshitasantra@gmail.com>
2025-12-18 22:27:40 -0600

    Initial Commit

---
+++ DITL/DITLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DITLApp.swift
+//  DITL
+//
+//  Created by Ashok Santra on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DITLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
